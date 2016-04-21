7c6bc49444f1e8967c4909598cae3e94e2778697
Simon Teague <simon_teague@live.co.uk>
2016-04-21 13:24:09 +0100

    Added getPersonView and countPostsInTopic

---
+++ src/uk/ac/bris/cs/databases/cwk3/API-simon-parts.java
@@ -0,0 +1,199 @@
+package uk.ac.bris.cs.databases.cwk3;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import sun.java2d.pipe.SpanShapeRenderer;
+import uk.ac.bris.cs.databases.api.APIProvider;
+import uk.ac.bris.cs.databases.api.AdvancedForumSummaryView;
+import uk.ac.bris.cs.databases.api.AdvancedForumView;
+import uk.ac.bris.cs.databases.api.ForumSummaryView;
+import uk.ac.bris.cs.databases.api.ForumView;
+import uk.ac.bris.cs.databases.api.AdvancedPersonView;
+import uk.ac.bris.cs.databases.api.PostView;
+import uk.ac.bris.cs.databases.api.Result;
+import uk.ac.bris.cs.databases.api.PersonView;
+import uk.ac.bris.cs.databases.api.SimpleForumSummaryView;
+import uk.ac.bris.cs.databases.api.SimpleTopicView;
+import uk.ac.bris.cs.databases.api.TopicView;
+
+/**
+ *
+ * @author csxdb
+ */
+public class API implements APIProvider {
+
+    private final Connection c;
+
+    public API(Connection c) {
+        this.c = c;
+    }
+
+    @Override
+    public Result<Map<String, String>> getUsers() {
+        final String statement = "SELECT * FROM Person";
+        Map<String, String> users = new HashMap<>();
+
+        try(PreparedStatement p = c.prepareStatement(statement)) {
+            ResultSet results = p.executeQuery();
+            while (results.next()) {
+                String username = results.getString("username");
+                String name = results.getString("name");
+
+                users.put(username, name);
+            }
+            results.close();
+            return Result.success(users);
+        } catch (SQLException e) {
+            return Result.fatal(e.getMessage());
+        }
+    }
+
+    @Override
+    public Result<PersonView> getPersonView(String username) {
+        final String statement = "SELECT * FROM Person WHERE username = ?";
+        PersonView pv;
+
+        try(PreparedStatement p = c.prepareStatement(statement)) {
+			p.setString(1, username);
+            ResultSet results = p.executeQuery();
+            while (results.next()) {
+                String name = results.getString("name");
+                String username = results.getString("username");
+                String studentId = results.getString("stuId");
+				
+				pv = new PersonView(name, username, studentId)
+            }
+            results.close();
+            return Result.success(pv);
+        } catch (SQLException e) {
+            return Result.fatal(e.getMessage());
+        }
+    }
+
+    @Override
+    public Result<List<SimpleForumSummaryView>> getSimpleForums() {
+//        throw new UnsupportedOperationException("Not supported yet.");
+        final String statement = "SELECT * FROM Forum ORDER BY title";
+        List<SimpleForumSummaryView> forums = new ArrayList<>();
+
+        try(PreparedStatement p = c.prepareStatement(statement)) {
+            ResultSet results = p.executeQuery();
+            while (results.next()) {
+                long id = results.getLong("id");
+                String title = results.getString("title");
+                SimpleForumSummaryView forum = new SimpleForumSummaryView(id, title);
+                forums.add(forum);
+            }
+            results.close();
+            return Result.success(forums);
+        } catch (SQLException e) {
+            return Result.fatal(e.getMessage());
+        }
+    }
+
+    @Override
+    public Result<Integer> countPostsInTopic(long topicId) {
+        final String statement = "SELECT COUNT(*) FROM Post WHERE Topic = ?";
+        int numRows;
+
+        try(PreparedStatement p = c.prepareStatement(statement)) {
+			p.setString(1, topicId);
+            ResultSet results = p.executeQuery();
+            while (results.next()) {
+				numRows = results.getInt(1);
+            }
+            results.close();
+            return Result.success(numRows);
+        } catch (SQLException e) {
+            return Result.fatal(e.getMessage());
+        }
+    }
+
+    @Override
+    public Result<List<PersonView>> getLikers(long topicId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Result<SimpleTopicView> getSimpleTopic(long topicId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Result<PostView> getLatestPost(long topicId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Result<List<ForumSummaryView>> getForums() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Result createForum(String title) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Result createPost(long topicId, String username, String text) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Result addNewPerson(String name, String username, String studentId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Result<ForumView> getForum(long id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Result<TopicView> getTopic(long topicId, int page) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Result likeTopic(String username, long topicId, boolean like) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Result favouriteTopic(String username, long topicId, boolean fav) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Result createTopic(long forumId, String username, String title, String text) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Result<List<AdvancedForumSummaryView>> getAdvancedForums() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Result<AdvancedPersonView> getAdvancedPersonView(String username) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Result<AdvancedForumView> getAdvancedForum(long id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Result likePost(String username, long topicId, int post, boolean like) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+   }
