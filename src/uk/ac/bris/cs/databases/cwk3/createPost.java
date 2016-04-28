c72c35f6fc1ba051674f998231aa25051abefb5a
Berk M <berkmolla@gmail.com>
2016-04-28 17:27:51 +0100

    CreatePost done, needs testing to see if it works.

--- src/uk/ac/bris/cs/databases/cwk3/createPost.java
+++ src/uk/ac/bris/cs/databases/cwk3/createPost.java
@@ -1,21 +1,16 @@
 package uk.ac.bris.cs.databases.cwk3;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
+import java.sql.*;
+import java.util.Date;
 import uk.ac.bris.cs.databases.api.Result;
-import uk.ac.bris.cs.databases.api.SimpleTopicSummaryView;
-import uk.ac.bris.cs.databases.api.ForumSummaryView;
-
-import java.util.List;
-import java.util.ArrayList;
 
 public class CreatePost {
     private Connection c;
-    private final String topicCheck = "SELECT * FROM Topic WHERE Topic.id = ?";
     private final String userCheck = "SELECT * FROM Person WHERE Person.username = ?";
+    private final String getPostsTable = " SELECT MAX(postNumber) as postNumber"
+                                       + " FROM Topic"
+                                       + " INNER JOIN Post ON Topic.id = Post.topic"
+                                       + " WHERE Topic.id = ?";
     private final String statement = "INSERT INTO Post" +
                                     " VALUES (null, ?, ?, ?, ?, ?)";
 
@@ -28,33 +23,44 @@ public class CreatePost {
         if (!validate(username, text)) {
             return Result.failure("Invalid input");
         }
-        try ( PreparedStatement p = c.prepareStatement(statement)) {
-            if (topicExists(topicID) && userExists(username)) {
-                p.setString(1, username);
-                p.setString(2, );
-                p.setString(3, studentId);
-                p.setString(4, studentId);
-                p.setString(5, text);
-                p.execute();
-                c.commit();
-                return Result.success();
-            }
-            else { return Result.failure("Username already exists"); }
+        try (PreparedStatement p = c.prepareStatement(statement)) {
+                Long userID = getUserIDForUsername(username);
+                if (userID == null) {
+                    return Result.failure("User does not exist!");
+                } else {
+                    int postNumber = getPostsTable(topicID);
+                    if (postNumber == 0) {
+                        return Result.failure("That topic does not exist!");
+                    }
+                    p.setString(1, username);
+                    p.setLong(2, topicID);
+                    p.setInt(3, (int) new Date().getTime());
+                    p.setInt(4, postNumber);
+                    p.setString(5, text);
+                    p.execute();
+                    c.commit();
+                    return Result.success();
+                }
         } catch (SQLException e) {
             return Result.fatal(e.getMessage());
         }
     }
 
-    private boolean topicExists(long topicID) throws SQLException {
-        try (PreparedStatement p = c.prepareStatement(topicCheck)) {
+    private int getPostsTable(long topicID) throws SQLException {
+        try (PreparedStatement p = c.prepareStatement(getPostsTable)) {
             p.setLong(1, topicID);
             try (ResultSet r = p.executeQuery()) {
-                return (r.isBeforeFirst());
+                if (!r.isBeforeFirst()) {
+                    return 0;
+                }
+                else {
+                    return r.getInt("postNumber");
+                }
             }
         }
     }
 
-    private Long userExists(String username) throws SQLException {
+    private Long getUserIDForUsername(String username) throws SQLException {
         try (PreparedStatement p = c.prepareStatement(userCheck)) {
             p.setString(1, username);
             try (ResultSet r = p.executeQuery()) {
